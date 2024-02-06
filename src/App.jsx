5a59a04c9b42bb5b81c664909a84d14309ddad73
Sabbir <sabbirhosainbd59@gmail.com>
2024-02-06 23:00:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+import "./App.css"
+import Header from "./Components/Header/Header.jsx"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,182 @@
+import { HiMiniBarsArrowDown } from 'react-icons/hi2'
+import { FaSearch, FaUser } from "react-icons/fa";
+import { IoMdCart } from "react-icons/io";
+
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">Navbar</a>
+
+          {/* icons for mobile devices */}
+          <div className='d-flex align-items-center d-lg-none ms-auto me-3'>
+
+            <div class="dropdown order-last">
+              <button class="btn btn-secondary dropdown-toggle dropdown_toggle_icon" data-bs-toggle="dropdown" aria-expanded="false"><FaUser /></button>
+              <ul class="dropdown-menu dropdown-menu-end">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </div>
+
+            <div className='me-3 order-2'>
+              <button class="btn btn-secondary position-relative">
+                <IoMdCart />
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
+              </button>
+            </div>
+
+            <div class="dropdown position-static me-3 order-1">
+              <button class="btn btn-secondary dropdown-toggle dropdown_toggle_icon" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                <FaSearch />
+              </button>
+              <form class="dropdown-menu w-100 p-4">
+                {/* <div class="input-group">
+                  <input type="email" class="form-control" id="" placeholder="Search hear..." />
+                  <button type="submit" class="btn btn-primary mt-3 w-100">Search</button>
+                </div> */}
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search items..." aria-describedby="search-box" />
+                  <button class="input-group-text bg-success" id="srarch-box">Search</button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"><HiMiniBarsArrowDown /></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+
+              {/* single dropdown */}
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Dropdown</a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+
+              {/* multi dropdown */}
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Multi Dropdown</a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  {/* ===== */}
+                  <li className="nav-item dropend">
+                    <a className="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Sub Dropdown</a>
+                    <ul className="dropdown-menu">
+                      <li><a className="dropdown-item" href="#">Action</a></li>
+                      <li><a className="dropdown-item" href="#">Another action</a></li>
+                      <li><a className="dropdown-item" href="#">Something else here</a></li>
+                      {/* ===== */}
+                      <li className="nav-item dropend">
+                        <a className="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Sub Dropdown</a>
+                        <ul className="dropdown-menu">
+                          <li><a className="dropdown-item" href="#">Action</a></li>
+                          <li><a className="dropdown-item" href="#">Another action</a></li>
+                          <li><a className="dropdown-item" href="#">Something else here</a></li>
+                          {/* ===== */}
+                          <li className="nav-item dropstart">
+                            <a className="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Sub Dropdown</a>
+                            <ul className="dropdown-menu">
+                              <li><a className="dropdown-item" href="#">Action</a></li>
+                              <li><a className="dropdown-item" href="#">Another action</a></li>
+                              <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+
+              {/* mega munu dropdown */}
+              <li className="nav-item dropdown position-static">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Mega Dropdown</a>
+                {/* mega menu list */}
+                <div className="dropdown-menu w-100">
+                  <div className="px-md-5">
+                    <div className="container-fluid">
+                      <div className="row">
+                        <div className="col-lg-3 col-md-6 col-sm-6 py-4">
+                          <h5>Hello World 1</h5>
+                          <div className="list-group">
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                          </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6 py-4">
+                          <h5>Hello World 1</h5>
+                          <div className="list-group">
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                          </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6 py-4">
+                          <h5>Hello World 1</h5>
+                          <div className="list-group">
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                          </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6 py-4">
+                          <h5>Hello World 1</h5>
+                          <div className="list-group">
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                            <a href="" className="list-group-item">Hello World</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href='#'>Disabled</a>
+              </li>
+
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
